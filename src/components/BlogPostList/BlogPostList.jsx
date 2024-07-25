b10a1880dacc4bee6c43b388c86684d487854596
Sushil Ingole <sushil.ingole@nihilent.com>
2024-07-25 14:00:00 +0530

    Error message handling

--- src/components/BlogPostList/BlogPostList.jsx
+++ src/components/BlogPostList/BlogPostList.jsx
@@ -16,6 +16,9 @@ const BlogPostList = () => {
                 if (response) {
                     const res = await response.json();
                     let { status, message } = res;
+                    res.articles = [];
+                    status = "error";
+                    message = "Requests from the browser are not allowed on the Developer plan, except from localhost.";
                     if (res?.articles?.length) {
                         setPosts(res.articles);
                         setAllBlogPost(res.articles);
@@ -33,15 +36,17 @@ const BlogPostList = () => {
         fetchData();
     }, [page]);
 
-    if (errorMsg) return
-    <div class="toast showing" role="alert" aria-live="assertive" aria-atomic="true">
-        <div class="toast-header">
-            <strong class="me-auto">Error</strong>
-        </div>
-        <div class="toast-body">
-            {errorMsg}
-        </div>
-    </div>
+    if (errorMsg) 
+        return (
+            <div class="toast showing" role="alert" aria-live="assertive" aria-atomic="true">
+                <div class="toast-header">
+                    <strong class="me-auto">Error</strong>
+                </div>
+                <div class="toast-body">
+                    {errorMsg}
+                </div>
+            </div>
+        );
     if (!allBlogPost?.length) return <Spinner />;
 
     return (
